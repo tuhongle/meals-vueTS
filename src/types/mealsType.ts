e10e2b696de91cd854a0f360ae8eba059053c23f
LE HONG TU <hongtufiroct@gmail.com>
2023-08-23 07:49:26 +0700

    adding dist

--- src/types/mealsType.ts
+++
@@ -1,7 +0,0 @@
-export type meals = { [key: string]: string };
-
-export type Response = { data: {meals: [ meals ]}} ;
-
-export type ResponseArray = { data: { meals:  meals[]}};
-
-export type ResponseIngredients = { data: { meals: {strIngredient: string}[] }};
